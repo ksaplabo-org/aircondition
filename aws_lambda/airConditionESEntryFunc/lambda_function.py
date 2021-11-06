f326a63f01f307e802724856fa926a11eb84b775
shinya.takada <s.takada.ksap.labo@gmail.com>
2021-11-06 20:18:13 +0900

    1st commit

---
+++ aws_lambda/airConditionESEntryFunc/lambda_function.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import os
+
+# ElasticSearch Entry
+HEADERS = {"Content-Type": "application/json"}
+URL = "http://" + os.environ["es_host"] + ":" + os.environ["es_port"]
+
+def lambda_handler(event, context):
+    # TODO implement
+    for record in event['Records']:
+        #データ取得
+        tm = record['dynamodb']['NewImage']['GetDateTime']['S']
+        tmp = record['dynamodb']['NewImage']['Temperature']['N']
+        hum = record['dynamodb']['NewImage']['Humidity']['N']
+        data={"GetDateTime":tm,"Humidity":float(hum),"Temperature":float(tmp)}
+
+        #ElasticSearchへ投入
+        r = requests.post(URL + "/" + os.environ["index"] + "/_doc" , headers=HEADERS, json=data)
+        print(r.text)
+    return {
+        'statusCode': r.status_code,
+        'body': r.text
+    }
+
+#Local Execute
+#testdata = {"Records":[{"dynamodb":{"NewImage":{"GetDateTime":{"S":"2021-11-03 16:20:18"},"Temperature":{"N":"20.5"},"Humidity":{"N":"72.3"}}}}]}
+#lambda_handler(testdata,"")
---
+++ aws_lambda/airConditionNotifyLineFunc/lambda_function.py
@@ -0,0 +1,31 @@
+import os
+import json
+import requests
+
+# Line Notify
+# 「access_token」を環境変数に設定してください。
+ACCESS_TOKEN = os.environ["access_token"]
+HEADERS = {"Authorization": "Bearer %s" % ACCESS_TOKEN,"Content-Type": "application/json"}
+URL = "https://api.line.me/v2/bot/message/broadcast"
+
+def lambda_handler(event, context):
+
+    for record in event['Records']:
+
+        #データ取得
+        tm = record['dynamodb']['NewImage']['GetDateTime']['S']
+        tm = "{0:s}時{1:s}分{2:s}秒".format(tm[11:13] ,tm[14:16] ,tm[17:19])
+        tmp = record['dynamodb']['NewImage']['Temperature']['N']
+        hum = record['dynamodb']['NewImage']['Humidity']['N']
+        payload = "測定時刻 {0}、気温 {1:.1f}℃、湿度 {2:.1f}%".format(tm ,float(tmp) ,float(hum))
+
+        #送信データ設定
+        send_data = {"messages": [{"type":"text","text":payload}]}
+
+        #lineに通知
+        r = requests.post(URL, headers=HEADERS, json=send_data)
+
+    return {
+        'statusCode': 200,
+        'body': 'HTTPStatusFromLineAPI:' + str(r.status_code)
+    }
